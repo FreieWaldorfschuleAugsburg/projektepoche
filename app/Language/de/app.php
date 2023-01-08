d966ea22dcb3e14b08871316d3dc6b3ea48a180f
elektr0nisch <contact@elektronisch.dev>
2023-01-08 17:04:54 +0100

    Add privacy policy link for the sake of the data protection guru

--- app/Language/de/app.php
+++ app/Language/de/app.php
@@ -7,7 +7,7 @@ return [
         'break' => 'Projektepoche der <br>Freien Waldorfschule Augsburg'
     ],
     'copyright' => [
-        'full' => '&copy; 2023 Freie Waldorfschule Augsburg<br><small>Entwickelt von <a href="https://elektronisch.dev" target="_blank">Linus Groschke und Tim Zolleis</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/projektepoche" target="_blank">Quellcode auf GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Impressum</a></small></small>',
+        'full' => '&copy; 2023 Freie Waldorfschule Augsburg<br><small>Entwickelt von <a href="https://elektronisch.dev" target="_blank">Linus Groschke und Tim Zolleis</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/projektepoche" target="_blank">Quellcode auf GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Impressum</a> – <a href="https://waldorf-augsburg.de/datenschutz" target="_blank">Datenschutz</a></small></small>',
         'short' => '&copy; 2023 Freie Waldorfschule Augsburg<br><small>Entwickelt von Linus Groschke und Tim Zolleis</small>'
     ]
 ];
