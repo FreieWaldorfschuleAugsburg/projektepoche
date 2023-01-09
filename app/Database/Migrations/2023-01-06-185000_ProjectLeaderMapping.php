191efbb5928e02d9bdaaa8b6666d12f028e2f0d0
tim <tim@zolleis.net>
2023-01-09 16:03:46 +0100

    fix migration error

--- app/Database/Migrations/2023-01-06-185000_ProjectLeaderMapping.php
+++ app/Database/Migrations/2023-01-06-185000_ProjectLeaderMapping.php
@@ -27,11 +27,11 @@ class ProjectLeaderMapping extends Migration
         $this->forge->addKey('id', true);
         $this->forge->addForeignKey('user_id', USERS, 'id');
         $this->forge->addForeignKey('project_id', PROJECTS, 'id');
-        $this->forge->createTable(LEADERS);
+        $this->forge->createTable(MEMBERS);
     }
 
     public function down()
     {
-        $this->forge->dropTable(LEADERS);
+        $this->forge->dropTable(MEMBERS);
     }
 }
