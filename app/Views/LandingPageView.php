345973d98e4c4d0a3d7e4b2343a3fe46c655ad13
elektr0nisch <contact@elektronisch.dev>
2023-01-22 18:30:13 +0100

    Update text on LandingPageView

--- app/Views/LandingPageView.php
+++ app/Views/LandingPageView.php
@@ -6,15 +6,23 @@
             </div>
             <div class="card-body">
                 <div class="text-center">
-                    <h1>Noch keine Informationen!</h1>
-                    <p>
-                        <!-- Coming soon text -->
-                        Derzeit gibt es noch keine genauen Informationen zur Projektepoche.
-                        <br/> Unten angezeigt werden die derzeit angemeldeten Projekte, <br/> die höchstwahrscheinlich bei der
-                        Projektepoche angeboten werden.
-                        <!-- Coming soon text end -->
-                    </p>
+                    <h1>Willkommen!</h1>
                 </div>
+                <p>
+                    Vom <b>27. Februar bis zum 10. März</b>, also in den zwei Wochen nach den Faschingsferien, wird der
+                    normale Stundenplan ausgesetzt und die Projektepoche stattfinden. In drei Zeitschienen werden
+                    viele verschiedene Kurse angeboten. In diesen zwei Wochen findet also kein normaler Unterricht
+                    statt, sondern drei von dir gewählte Kurse.
+                    <br/>
+                    <br/>
+                    Von <b>Montag bis Freitag (23.1. - 27.1.)</b> könnt Ihr die Kurse wählen. Da viele Kurse eine
+                    Teilnehmerbegrenzung haben, gebt Ihr für jede Zeitschiene eine 1., 2. und 3. Wahl an - also
+                    insgesamt 9 Kurse. Die Wahl findet online statt. Die Informationen zur Anmeldung erhältst du auf
+                    dem Blatt zusammen mit deinem persönlichen Passwort. Bewahre dieses Blatt bitte sorgfältig auf!
+                    <br/>
+                    <br/>
+                    Wir wünschen viel Freude beim Wählen und hoffen, dass für jeden etwas Interessantes dabei ist.
+                </p>
             </div>
         </div>
     </div>
